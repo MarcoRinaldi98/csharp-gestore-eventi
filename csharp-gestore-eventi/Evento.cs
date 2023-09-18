714b8757d20ccb7912186a11282bd1d44450405d
Marco Rinaldi <marcorinaldi98@libero.it>
2023-09-18 09:39:25 +0200

    Aggiungere i file di progetto.

---
+++ csharp-gestore-eventi/Evento.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    public class Evento
+    {
+        public string Titolo { get; private set; }
+        public DateTime Data { get; private set; }
+        public int NumeroPostiMassimi { get; }
+        public int NumeroPostiPrenotati { get; }
+
+        public Evento(string titolo, DateTime data, int numeroPostiMassimi , int numeroPostiPrenotati)
+        {
+            this.Titolo = titolo;
+            this.Data = data;
+            this.NumeroPostiMassimi = numeroPostiMassimi;
+            this.NumeroPostiPrenotati = numeroPostiPrenotati;
+        }
+    }
+}
