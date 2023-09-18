00cb3ff16ed359bb138edc7511d62737835842ab
Marco Rinaldi <marcorinaldi98@libero.it>
2023-09-18 10:58:55 +0200

    creata classe ProgrammaEventi con attributi, getter, setter e costruttore

---
+++ csharp-gestore-eventi/ProgrammaEventi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    public class ProgrammaEventi
+    {
+        // ATTRIBUTI
+        public string Titolo { get; set; }
+        List<Evento> eventi;
+
+        // COSTRUTTORE
+        public ProgrammaEventi(string titolo) 
+        {
+            this.Titolo = titolo;
+            eventi = new List<Evento>();
+        }
+    }
+}
