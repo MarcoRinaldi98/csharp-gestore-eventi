ae67b74d6465c096b2540888cf78f22c581b3814
Marco Rinaldi <marcorinaldi98@libero.it>
2023-09-18 10:23:42 +0200

    creati metodi PrenotaPosti(), DisdiciPosti() e fatto override del metodo ToString()

--- csharp-gestore-eventi/Evento.cs
+++ csharp-gestore-eventi/Evento.cs
@@ -47,33 +47,60 @@ namespace csharp_gestore_eventi
                 }
             }
         }
-        private int numeroPostiMassimi;
-        public int NumeroPostiMassimi 
-        { 
-            get
-            {
-                return numeroPostiMassimi;
-            }
-            private set
-            {
-                if (value < 0)
-                {
-                    throw new ArgumentOutOfRangeException(null, "Il numero di posti non può essere inferiore a zero!");
-                }
-                else
-                {
-                    numeroPostiMassimi = value;
-                }
-            }
-        }
+        public int NumeroPostiMassimi { get; private set; }
         public int NumeroPostiPrenotati { get; private set; }
 
         public Evento(string titolo, DateTime data, int numeroPostiMassimi)
         {
             this.Titolo = titolo;
             this.Data = data;
-            this.NumeroPostiMassimi = numeroPostiMassimi;
+            if(numeroPostiMassimi < 0)
+            {
+                throw new ArgumentException("La capienza massima deve essere un numero positivo.");
+            } else
+            {
+                this.NumeroPostiMassimi = numeroPostiMassimi;
+            }
             this.NumeroPostiPrenotati = 0;
+        }
+
+        public void PrenotaPosti(int numeroPosti)
+        {
+            if (Data <= DateTime.Now )
+            {
+                throw new InvalidOperationException("Impossibile prenotare posti per un evento passato.");
+            }
+            if (numeroPosti <= 0)
+            {
+                throw new ArgumentException("Il numero di posti da prenotare deve essere maggiore di zero.");
+            }
+            if (NumeroPostiPrenotati + numeroPosti > NumeroPostiMassimi)
+            {
+                throw new InvalidOperationException($"Non ci sono abbastanza posti disponibili per questo evento, sono ancora disponibili {NumeroPostiMassimi - NumeroPostiPrenotati} posti.");
+            }
+            NumeroPostiPrenotati += numeroPosti;
+        }
+
+        public void DisdiciPosti(int numeroPosti)
+        {
+            if (Data <= DateTime.Now)
+            {
+                throw new InvalidOperationException("Impossibile disdire posti per un evento passato.");
+            }
+            if (numeroPosti <= 0)
+            {
+                throw new ArgumentException("Il numero di posti da disdire deve essere maggiore di zero.");
+            }
+            if (NumeroPostiPrenotati < numeroPosti)
+            {
+                throw new InvalidOperationException($"Non ci sono abbastanza posti prenotati da disdire, sono stati prenotati {NumeroPostiPrenotati} posti");
+            }
+            NumeroPostiPrenotati -= numeroPosti;
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString("dd/MM/yyyy") + " - " + Titolo;
         }
     }
 }
